e2f262077f14cdaad594dbe3d2ba0e4e22bb254c
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2022-02-10 21:48:06 +0300

    updated shop data with new data

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cartTypes';
+import { addItemToCart } from './cartUtils';
 
 const INITIAL_STATE = {
   hidden: false,
@@ -10,7 +11,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
     case CartActionTypes.ADD_ITEM:
-      return { ...state, cartItems: [...state.cartItems, action.payload] };
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
